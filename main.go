feac971779702d6865e763abcb6c786633e89278
Marcus Östling <mpt.ostling@gmail.com>
2025-12-08 20:14:32 +0100

    Add CLI argument handling with -i and -v flags

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, Lin Router!")
-	_, err := ParsePcbFile("main.kicad_pcb")
+	inputPath := flag.String("i", "", "Path to the KiCad PCB file to process (required)")
+	verbose := flag.Bool("v", false, "Enable verbose output")
+	flag.Parse()
+
+	if *inputPath == "" {
+		fmt.Println("Error: -i flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *verbose {
+		fmt.Println("Hello, Lin Router!")
+	}
+
+	_, err := ParsePcbFile(*inputPath)
 	if err != nil {
 		fmt.Println("Error parsing PCB file:", err)
-	} else {
-		// fmt.Println("Parsed PCB:", PCB)
+		os.Exit(1)
 	}
 }
